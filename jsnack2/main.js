4fb10960e4e10d3894d499443a85e6e41b9b871b
paoloevan <paoloev1900@gmail.com>
2022-10-12 12:05:39 +0200

    add jsnack2

---
+++ jsnack2/main.js
@@ -0,0 +1,17 @@
+/*
+L’utente inserisce due parole in successione, con due prompt.
+Il software stampa prima la parola più corta, poi la parola più lunga.
+*/
+
+const firstWorld = prompt('Inserisci prima parola')
+const secondWorld = prompt('Inserisci seconda parola')
+
+if (firstWorld.length > secondWorld.length) {
+    console.log(firstWorld);
+    console.log(secondWorld);
+} else if (firstWorld.length < secondWorld.length) {
+    console.log(secondWorld);
+    console.log(firstWorld);
+} else {
+    console.log('le parole sono lunghe uguali');
+}
