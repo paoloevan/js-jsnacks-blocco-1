aa66946961d4b8879a4de75cd1b777a14c61c014
paoloevan <paoloev1900@gmail.com>
2022-10-13 12:04:47 +0200

    add jsnack3 with while

--- jsnack3/main.js
+++ jsnack3/main.js
@@ -3,7 +3,7 @@ Il software deve chiedere per 10 volte all’utente di inserire un numero.
 Il programma stampa la somma di tutti i numeri inseriti.
 */
 
-let totNumberEl;
+let totNumberEl = 0;
 let numberEl;
 
 for (let i = 1; i < 11; i++) {
@@ -11,3 +11,22 @@ for (let i = 1; i < 11; i++) {
     totNumberEl += numberEl;
 }
 console.log(totNumberEl);
+
+/*
+con While:
+Il software deve chiedere per 10 volte all’utente di inserire un numero.
+Il programma stampa la somma di tutti i numeri inseriti. 
+*/
+
+let totNumberElWhile = 0;
+let numberElWhile;
+
+let index = 1;
+while (index < 11) {
+    numberElWhile = Number(prompt('Inserisci numero ' + index));
+    totNumberElWhile += numberElWhile;
+
+    index++
+}
+
+console.log('con While', totNumberElWhile);
